2ff0f56c883b55c2b2f6a35de140f903e7f0a573
石井佑樹 <ishiiyuuki@ishiiyuukinoMacBook-Air.local>
2020-05-10 04:08:30 +0900

    fix ron

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,11 @@ const OBSTACLE_HEIGHT: f32 = 302.;
 const ROCK_HEIGHT: f32 = 52.;
 const GRAVITY: f32 = -0.5;
 
-
-//ゲームを実行するシステム
+/// ゲームを実行するシステム
 pub struct PlaySystem;
 
-//システムを作るときはSystemのTraitを用いる
-impl<'a> System<'a> for PlaySystem{
+/// システムを作るときはSystemのTraitを用います
+impl<'a> System<'a> for PlaySystem {
     // このシステムで使用するComponentの種類
     type SystemData = (
         WriteStorage<'a, Transform>, // TransformはEntityの座標やサイズをそうさするComponentへの書き込み
@@ -38,36 +37,33 @@ impl<'a> System<'a> for PlaySystem{
         Read<'a, InputHandler<StringBindings>>, // ユーザーからの入力に関するComponentを読み込み
         Read<'a, Time>,              // 時間Componentの読み込み
     );
-    
-    //システムの実行関数
+
+    /// システムの実行関数
     fn run(&mut self, (mut transforms, mut rocks, mut obstacles, input, time): Self::SystemData) {
         // joinによってEntityを共有しているComponentの集合を得ることが可能
-        for (transform,rock) in (&mut transforms, &mut rocks).join()
-        {
-            //前フレームからの経過時間を取得
+        for (transform, rock) in (&mut transforms, &mut rocks).join() {
+            // 前のフレームからの時間経過を取得
             let dt = time.delta_real_seconds() * 70.;
+
             // Enterキーの入力を検知
-            if input.key_is_down(VirtualKeyCode::Return){
-                //
+            if input.key_is_down(VirtualKeyCode::Return) {
+                // 上向きへ速度を与えることでジャンプ
                 rock.set_velocity(7.);
             }
 
-             // 基本的には下向きへの加速
-
-             let mut new_velocity = rock.velocity + GRAVITY * dt;
-             let mut new_y = rock.y + new_velocity * dt;
-
-             // 地面についたら速度が0になります
+            // 基本的には下向きへの加速
+            let mut new_velocity = rock.velocity + GRAVITY * dt;
+            let mut new_y = rock.y + new_velocity * dt;
+            // 地面についたら速度が0になります
             if new_y <= ROCK_HEIGHT / 2. {
                 new_velocity = 0.;
                 new_y = ROCK_HEIGHT / 2.;
             }
 
-             // 実際に岩エンティティの座標を変更し、岩の情報も更新
-             transform.set_translation_y(new_y);
-             rock.set_y(new_y);
-             rock.set_velocity(new_velocity);
-
+            // 実際に岩エンティティの座標を変更し、岩の情報も更新
+            transform.set_translation_y(new_y);
+            rock.set_y(new_y);
+            rock.set_velocity(new_velocity);
         }
 
         // 同様に障害物のComponentを取得
@@ -86,42 +82,44 @@ impl<'a> System<'a> for PlaySystem{
     }
 }
 
-
 /// ゲーム画面のState
 struct PlayState;
 
+/// SimpleStateというTraitを使用
 impl SimpleState for PlayState {
+    /// PlayStateへ遷移した瞬間に実行
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
-        //EntityをWorldに追加
+        // EntityをWorldに追加します
         let sprite_sheet_handle = load_sprite_sheet(data.world);
         set_camera(data.world);
-        set_rock(data.world,sprite_sheet_handle.clone());
-        set_obstacle(data.world,sprite_sheet_handle);
+        set_rock(data.world, sprite_sheet_handle.clone());
+        set_obstacle(data.world, sprite_sheet_handle);
     }
 
-    // PlayStateがPopされるときに実行される
-    fn on_stop(&mut self , data: StateData<'_, GameData<'_, '_>>){
+    /// PlayStateがPopされるときに実行
+    fn on_stop(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         data.world.delete_all();
     }
 }
 
-pub fn set_camera(world: &mut World){
-    let mut camera_transform = Transform::default(); // カメラの位置を調整するためのComponent
-    camera_transform.set_translation_xyz(SCREEN_WIDTH / 2., SCREEN_HEIGHT / 2., 1.0); //スクリーンの中心に設置、高さは1.0のところ
+/// カメラEntityをWorldに追加
+pub fn set_camera(world: &mut World) {
+    let mut camera_transform = Transform::default();
+    camera_transform.set_translation_xyz(SCREEN_WIDTH / 2., SCREEN_HEIGHT / 2., 1.0);
     world
         .create_entity()
         .with(camera_transform)
         .with(Camera::standard_2d(SCREEN_WIDTH, SCREEN_HEIGHT))
-        .build(); 
+        .build();
 }
 
-/// 岩EntityをWorldに追加します
+/// 岩EntityをWorldに追加
 pub fn set_rock(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
     let mut rock_transform = Transform::default();
     rock_transform.set_translation_xyz(SCREEN_WIDTH / 4., 0., 0.);
     let rock_sprite_render = SpriteRender {
         sprite_sheet: sprite_sheet_handle,
-        sprite_number: 0, //SpriteSheet中の画像の1つ目
+        sprite_number: 0,
     };
 
     world
@@ -132,25 +130,24 @@ pub fn set_rock(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
         .build();
 }
 
-/// 障害物EntityをWorldに追加します
-pub fn set_obstacle(world: &mut World, sprite_sheet_handle : Handle<SpriteSheet>){
-
+/// 障害物EntityをWorldに追加
+pub fn set_obstacle(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
     let mut obstacle_transform = Transform::default();
-    obstacle_transform.set_translation_xyz(SCREEN_HEIGHT -10. , OBSTACLE_HEIGHT /2. - 30. ,0.);
-    let obstacle_sprite_render = SpriteRender{
+    obstacle_transform.set_translation_xyz(SCREEN_HEIGHT - 10., OBSTACLE_HEIGHT / 2. - 30., 0.);
+    let obstacle_sprite_render = SpriteRender {
         sprite_sheet: sprite_sheet_handle,
         sprite_number: 1,
     };
-
-    world.create_entity()
+    world
+        .create_entity()
         .with(obstacle_transform)
         .with(Obstacle::new())
         .with(obstacle_sprite_render)
         .build();
 }
 
-// 2Dゲームに必要な画像をまとめたSpriteSheetをロード
-pub fn load_sprite_sheet(world: &World) -> Handle<SpriteSheet>{
+/// 2Dゲームに必要な画像をまとめたファイルSpriteSheetをロード
+pub fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
     let loader = world.read_resource::<Loader>();
     let texture_handle = {
         let texture_storage = world.read_resource::<AssetStorage<Texture>>();
@@ -171,19 +168,17 @@ pub fn load_sprite_sheet(world: &World) -> Handle<SpriteSheet>{
     )
 }
 
-
-
 /// 岩の情報を保持するComponent
-pub struct Rock{
+pub struct Rock {
     y: f32,
     velocity: f32,
 }
 
-impl Rock{
-    pub fn new() -> Rock{
-        Rock{
-            y: 100. ,
-            velocity: 0. ,
+impl Rock {
+    pub fn new() -> Rock {
+        Rock {
+            y: 100.,
+            velocity: 0.,
         }
     }
 
@@ -201,44 +196,44 @@ impl Component for Rock {
     type Storage = DenseVecStorage<Self>;
 }
 
-
-
 /// 障害物の情報を保持するComponent
 pub struct Obstacle {
-    x: f32, // 障害物は右から左にいくだけなのでx座標のみ
+    x: f32,
 }
 
 impl Obstacle {
-    pub fn new() -> Obstacle{
-        Obstacle{
-            x: SCREEN_HEIGHT -10. ,
+    pub fn new() -> Obstacle {
+        Obstacle {
+            x: SCREEN_HEIGHT - 10.,
         }
     }
 
-    pub fn set_x(&mut self, new_x: f32){
+    pub fn set_x(&mut self, new_x: f32) {
         self.x = new_x;
     }
 }
+
 /// ComponentというTraitを用いてObstacleをComponentにする
 impl Component for Obstacle {
     type Storage = DenseVecStorage<Self>;
 }
-
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
     let app_root = application_root_dir()?;
 
-    //スクリーンサイズなどの設定ファイルのPath
-    let assets_dir = app_root.join("assets");
+    // スクリーンサイズなどの設定ファイルのパス
     let config_dir = app_root.join("config");
     let display_config_path = config_dir.join("display.ron");
 
-    //入力システム
+    // アセットのパス
+    let assets_dir = app_root.join("assets");
+
+    // 入力システム
     let input_bundle = InputBundle::<StringBindings>::new();
 
-    //ゲームデータ作成　システム　設定を追加
+    // ゲームのデータを作成し、システムや設定を追加していく
     let game_data = GameDataBuilder::default()
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
@@ -246,7 +241,7 @@ fn main() -> amethyst::Result<()> {
                     RenderToWindow::from_config_path(display_config_path)?
                         .with_clear([0.34, 0.36, 0.52, 1.0]),
                 )
-                .with_plugin(RenderFlat2D::default()),//フラットシェーディング
+                .with_plugin(RenderFlat2D::default()), //フラットシェーディング
         )?
         .with_bundle(TransformBundle::new())?
         .with_bundle(input_bundle)?
@@ -255,6 +250,7 @@ fn main() -> amethyst::Result<()> {
     // アセットのパスと初期Stateとゲームデータによってゲームを作成
     let mut game = Application::new(assets_dir, PlayState, game_data)?;
 
+    // ゲームの実行
     game.run();
 
     Ok(())
